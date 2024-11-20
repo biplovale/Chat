6ab7f0e7105ac8f15b95335981004104ee6685b1
Biplov Ale <biplovale2020@gmail.com>
2024-11-20 13:21:05 -0500

    initial api controller set up and swagger integration

---
+++ src/ChatApi.Api/Controllers/DashBoardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApi.Controllers
+{
+    /// <summary>
+    /// Represents a controller for handling DashBoard requests.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class DashBoardController : Controller
+    {
+        /// <summary>
+        /// Gets something
+        /// </summary>
+        /// <returns>The something.</returns>
+        [HttpGet("/something")]
+        public IActionResult Index()
+        {
+            return Ok("something");
+        }
+    }
+}
